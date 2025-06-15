68843fa25f8ccc8bcb0c4203f185d6458098df7b
joominkyu <empal03@gmail.com>
2025-06-15 22:46:28 +0900

    fix: clsx 라이브러리 삭제 및 tailwind로 color 동적 변경

--- app/(main)/components/RankBox.tsx
+++ app/(main)/components/RankBox.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import { userProps } from "../props/userProps";
-import clsx from "clsx";
 
 export default function RankBox({ data, color }: { data: userProps, color:string}) {
-    const borderColor = `border-${color}`;
-    const bgColor = `bg-${color}`;
-
     return (
-        <div className={clsx("w-full max-w-[235px] flex flex-col items-center justify-center border rounded-[16px]", borderColor)}>
-            <p className={clsx("w-full p-[8px] border-b-[2px] text-[14px]", borderColor)}>무릉도장 1위</p>
+        <div className={`w-full max-w-[235px] flex flex-col items-center justify-center border border-${color} rounded-[16px]`}>
+            <p className={`w-full p-[8px] border-b-[2px] text-[14px] border-${color}`}>무릉도장 1위</p>
             <div className="w-full flex gap-[4px] items-center justify-center text-[12px] p-[8px] pt-[12px]">
                 <p>{data.name}</p>
                 <p>Lv.{data.level}</p>
@@ -23,7 +19,7 @@ export default function RankBox({ data, color }: { data: userProps, color:string
                     <span className="text-[12px]">({data.trophyScore})</span>
                 )}
             </p>
-            <div className={clsx("w-full border-t font-bold p-[8px] rounded-b-[16px]", borderColor, bgColor)}>
+            <div className={`w-full border-t font-bold p-[8px] rounded-b-[16px] border-${color} bg-${color}`}>
                 상세보기
             </div>
         </div>
