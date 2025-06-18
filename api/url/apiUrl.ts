3fcdadf8bffae63591b2b65aa98f629bcecf3973
joominkyu <empal03@gmail.com>
2025-06-18 18:32:03 +0900

    feat: 유저 검색 시 기본 정보 노출

--- api/url/apiUrl.ts
+++ api/url/apiUrl.ts
@@ -1,22 +1,44 @@
-import { getToDate } from '@/api/getDate';
+import { getToDate, getYdayDate } from '@/api/getDate';
+
+const baseUrl = 'https://open.api.nexon.com';
 
 // rank
-export const dojangUrl = `https://open.api.nexon.com/maplestory/v1/ranking/dojang?date=${getToDate()}&difficulty=1`; // 무릉도장
-export const theseedUrl = `https://open.api.nexon.com/maplestory/v1/ranking/theseed?date=${getToDate()}`; // 더시드
-export const achievementUrl = `https://open.api.nexon.com/maplestory/v1/ranking/achievement?date=${getToDate()}`; // 업적
+export const dojangUrl = `${baseUrl}/maplestory/v1/ranking/dojang?date=${getToDate()}&difficulty=1`; // 무릉도장
+export const theseedUrl = `${baseUrl}/maplestory/v1/ranking/theseed?date=${getToDate()}`; // 더시드
+export const achievementUrl = `${baseUrl}/maplestory/v1/ranking/achievement?date=${getToDate()}`; // 업적
 
 // user
 export const ocidUrl = (name: string) => {
     // ocid (유저 고유 키 값)
-    const url = `https://open.api.nexon.com/maplestory/v1/id?character_name=${name}`;
+    const url = `${baseUrl}/maplestory/v1/id?character_name=${name}`;
     return url;
 };
 export const userUrl = (ocid: string) => {
     // 유저 정보 (레벨, 닉네임 등등)
-    const url = `https://open.api.nexon.com/maplestory/v1/character/basic?ocid=${ocid}`;
+    const url = `${baseUrl}/maplestory/v1/character/basic?ocid=${ocid}`;
+    return url;
+};
+export const popularityUrl = (ocid: string) => {
+    // 유저 인기도
+    const url = `${baseUrl}/maplestory/v1/character/popularity?ocid=${ocid}&date=${getYdayDate()}`;
+    return url;
+};
+export const overallUrl = (ocid: string, world?: string) => {
+    // 유저 랭킹
+    const url = `${baseUrl}/maplestory/v1/ranking/overall?date=${getToDate()}&ocid=${ocid}${world ? `&world_name=${world}` : ''}`;
+    return url;
+};
+export const unionUrl = (ocid: string) => {
+    // 유저 유니온 기본 정보
+    const url = `${baseUrl}/maplestory/v1/user/union?ocid=${ocid}&date=${getYdayDate()}`;
+    return url;
+};
+export const statUrl = (ocid: string) => {
+    // 유저 스탯 정보
+    const url = `${baseUrl}/maplestory/v1/character/stat?ocid=${ocid}&date=${getYdayDate()}`;
     return url;
 };
 
 // notice
-export const noticeUrl = `https://open.api.nexon.com/maplestory/v1/notice`; // 공지
-export const updateUrl = `https://open.api.nexon.com/maplestory/v1/notice-update`; // 업데이트
+export const noticeUrl = `${baseUrl}/maplestory/v1/notice`; // 공지
+export const updateUrl = `${baseUrl}/maplestory/v1/notice-update`; // 업데이트
---
+++ app/user/[name]/components/UserHeader.tsx
@@ -0,0 +1,66 @@
+import { userDataProps, userStatProps } from "../../userProps/props";
+import Image from "next/image";
+import { popularityUrl, overallUrl, unionUrl, statUrl } from "@/api/url/apiUrl";
+import ssrFetcher from "@/api/ssrFetcher";
+
+export default async function UserHeader({ data, ocid }: { data: userDataProps, ocid: string}) {
+    // data는 기본 정보
+    // 인기도 정보
+    const userPopularityUrl = popularityUrl(ocid);
+    const userPopularity = await ssrFetcher(userPopularityUrl);
+
+    // 랭킹
+    const userOverallUrl = overallUrl(ocid); // 전체 랭킹
+    const userOverallData = await ssrFetcher(userOverallUrl);
+    const userOverall = userOverallData[0].ranking[0].ranking.toLocaleString();
+
+    const userWorldOverallUrl = overallUrl(ocid, data.worldName); // 월드 랭킹
+    const userWorldOverallData = await ssrFetcher(userWorldOverallUrl);
+    const userWorldOverall = userWorldOverallData[0].ranking[0].ranking.toLocaleString();
+    
+    // 유니온
+    const userUnionUrl = unionUrl(ocid);
+    const userUnionData = await ssrFetcher(userUnionUrl);
+    const userUnionLevel = userUnionData[0].union_level.toLocaleString();
+
+    // 전투력 정보
+    const userStatUrl = statUrl(ocid);
+    const userStatData = await ssrFetcher(userStatUrl);
+    const finalStat = userStatData[0].final_stat;
+    const combatStatData = finalStat.find((stat: userStatProps) => stat.stat_name === "전투력");
+    const combatStat = Number(combatStatData.stat_value).toLocaleString();
+
+    return (
+        <div className="w-full flex items-center justify-start">
+            <Image src={data.characterImage} alt={data.characterName} width={144} height={144} className="mx-[30px]" />
+            <div className="flex flex-col gap-[12px]">
+                <div>
+                    <div className="flex gap-[6px] text-[13px] text-[#f9f9f9] font-bold mb-[6px]">
+                        <p className="py-[4px] px-[10px] bg-neutral-400 rounded-[8px]">{data.worldName}</p>
+                        <p className="py-[4px] px-[10px] bg-neutral-400 rounded-[8px]">{data.characterClass}</p>
+                    </div>
+                    <p className="text-[24px] font-bold">{data.characterName}</p>
+                </div>
+                <div className="flex flex-col gap-[3px] text-[14px] font-semibold">
+                    <p>길드 {data.characterGuildName ? data.characterGuildName : '-' }</p>
+                    <p>인기도 {userPopularity[0].popularity}</p>
+                    <p>종합랭킹 {userOverall}위 ({userWorldOverall}위)</p>
+                </div>
+                <div className="flex gap-[16px]">
+                    <div>
+                        <p>레벨</p>
+                        <p className="text-[20px] font-bold">LV.{data.characterLevel}</p>
+                    </div>
+                    <div>
+                        <p>유니온</p>
+                        <p className="text-[20px] font-bold">{userUnionLevel}</p>
+                    </div>
+                    <div>
+                        <p>전투력</p>
+                        <p className="text-[20px] font-bold">{combatStat}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
