50d3955968ad912404bc968ec27c2550e785e0dd
joominkyu <empal03@gmail.com>
2025-10-31 18:27:46 +0900

    fix: 로고 클릭 시 홈으로 이동하도록 링크 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import ThemeBtn from "./header/ThemeBtn";
+import Link from "next/link";
 // import NavBar from "./header/NavBar";
 
 export default function Header() {
 	return (
 		<div className="w-full max-w-[940px] m-auto pt-[16px] flex flex-col gap-[16px]">
             <div className="w-full flex items-center justify-between">
-            	<Image src={"/logo.png"} alt={"단풍지지"} width={120} height={25} className="dark:invert" />
+            	<Link href='/'><Image src={"/logo.png"} alt={"단풍지지"} width={120} height={25} className="dark:invert" /></Link>
 				<ThemeBtn />
 			</div>
 			{/* <NavBar /> */}
