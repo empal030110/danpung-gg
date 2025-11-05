1060569969486b5f3102018006f020babceab6dd
joominkyu <empal03@gmail.com>
2025-11-05 19:26:08 +0900

    feat: 장비 잠재능력 추가

--- app/user/[name]/components/ItemBox.tsx
+++ app/user/[name]/components/ItemBox.tsx
@@ -34,10 +34,26 @@ export default function ItemBox({item = [], android = false, title = false}: {it
 
     return (
         <div className="border border-neutral-400 p-[12px] rounded-[8px] mb-[8px]">
-            <div className="flex justify-start items-center gap-[8px] h-full">
+            <div className={`flex justify-start items-center gap-[8px] ${filterItem.potential_option_grade ? 'mb-[8px]' : 'h-full'}`}>
                 <Image src={filterItem?.item_icon || ""} alt={filterItem?.item_name || ""} width={30} height={30} loading="lazy" />
                 <p className="text-[14px]">{filterItem.item_name}</p>
             </div>
+            {filterItem.potential_option_grade && (
+                <div className="flex flex-wrap text-[12px] gap-[4px]">
+                    <p>잠재</p>
+                    {filterItem.potential_option_1 && (<p className={`${filterItem.potential_option_grade === '레전드리' ? 'text-green-400' : filterItem.potential_option_grade === '유니크' ? 'text-yellow-400' : filterItem.potential_option_grade === '에픽' ? 'text-purple-400' : 'text-blue-300'}`}>{filterItem.potential_option_1}</p>)}
+                    {filterItem.potential_option_2 && (<p className={`${filterItem.potential_option_grade === '레전드리' ? 'text-green-400' : filterItem.potential_option_grade === '유니크' ? 'text-yellow-400' : filterItem.potential_option_grade === '에픽' ? 'text-purple-400' : 'text-blue-300'}`}>{filterItem.potential_option_2}</p>)}
+                    {filterItem.potential_option_3 && (<p className={`${filterItem.potential_option_grade === '레전드리' ? 'text-green-400' : filterItem.potential_option_grade === '유니크' ? 'text-yellow-400' : filterItem.potential_option_grade === '에픽' ? 'text-purple-400' : 'text-blue-300'}`}>{filterItem.potential_option_3}</p>)}
+                </div>
+            )}
+            {filterItem.additional_potential_option_grade && (
+                <div className="flex flex-wrap text-[12px] gap-[4px] mt-[4px]">
+                    <p>에디</p>
+                    {filterItem.additional_potential_option_1 && (<p className={`${filterItem.additional_potential_option_grade === '레전드리' ? 'text-green-400' : filterItem.additional_potential_option_grade === '유니크' ? 'text-yellow-400' : filterItem.additional_potential_option_grade === '에픽' ? 'text-purple-400' : 'text-blue-300'}`}>{filterItem.additional_potential_option_1}</p>)}
+                    {filterItem.additional_potential_option_2 && (<p className={`${filterItem.additional_potential_option_grade === '레전드리' ? 'text-green-400' : filterItem.additional_potential_option_grade === '유니크' ? 'text-yellow-400' : filterItem.additional_potential_option_grade === '에픽' ? 'text-purple-400' : 'text-blue-300'}`}>{filterItem.additional_potential_option_2}</p>)}
+                    {filterItem.additional_potential_option_3 && (<p className={`${filterItem.additional_potential_option_grade === '레전드리' ? 'text-green-400' : filterItem.additional_potential_option_grade === '유니크' ? 'text-yellow-400' : filterItem.additional_potential_option_grade === '에픽' ? 'text-purple-400' : 'text-blue-300'}`}>{filterItem.additional_potential_option_3}</p>)}
+                </div>
+            )}
         </div>
     );
 }
