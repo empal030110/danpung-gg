ad11d5490926ea2a81f552292b4858024f45ab24
joominkyu <empal03@gmail.com>
2026-08-10 21:31:51 +0900

    fix: 메타데이터 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,16 +15,16 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "empal03 - 단풍지지 - 메이플스토리 통계 & 분석 플랫폼",
-    description: "empal03 - 메이플스토리 직업 통계, 길드 분석, 유저 정보 등을 제공하는 단풍지지 플랫폼입니다.",
+    title: "단풍지지 - 메이플스토리 통계 & 분석 플랫폼",
+    description: "메이플스토리 직업 통계, 길드 분석, 유저 정보 등을 제공하는 단풍지지 플랫폼입니다.",
     keywords: ["메이플스토리", "직업 통계", "길드 분석", "단풍지지", "MapleStory", "danpungGG"],
-	authors: [{ name: "empal03", url: "https://maple-gg.vercel.app" }],
+	  authors: [{ name: "empal03", url: "https://maple-gg.vercel.app" }],
     creator: "empal03",
     publisher: "empal03",
 
     openGraph: {
-		title: "empal03 - 단풍지지 - 메이플스토리 통계 & 분석 플랫폼",
-		description: "empal03 - 메이플스토리 직업 통계, 길드 분석, 유저 정보 등을 제공하는 단풍지지 플랫폼입니다.",
+		title: "단풍지지 - 메이플스토리 통계 & 분석 플랫폼",
+		description: "메이플스토리 직업 통계, 길드 분석, 유저 정보 등을 제공하는 단풍지지 플랫폼입니다.",
 		url: "https://maple-gg.vercel.app",
 		siteName: "단풍지지",
 		locale: "ko_KR",
