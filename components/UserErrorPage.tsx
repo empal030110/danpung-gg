bb743c7c4a8ae3d8b20ae8a3256d142d96f0347b
joominkyu <empal03@gmail.com>
2026-08-10 21:49:55 +0900

    feat: 에러 페이지에 새로고침 버튼 추가

--- components/UserErrorPage.tsx
+++ components/UserErrorPage.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from "next/link";
 import { FaUserAltSlash } from "react-icons/fa";
 
@@ -8,7 +10,8 @@ export default function UserErrorPage() {
                 <div className="mb-[16px]"><FaUserAltSlash size={64} /></div>
                 <h2>정보를 불러오지 못했어요.</h2>
                 <p className="mt-[8px]">오타가 있거나 정지된 캐릭터일 수 있습니다. (영어 대소문자 구분 필요)</p>
-                <div className="mt-[32px]">
+                <div className="mt-[32px] flex gap-[8px]">
+                    <button onClick={() => window.location.reload()} className="p-[12px] bg-gray-400 rounded-[12px] text-black text-[14px] cursor-pointer">새로고침</button>
                     <Link href='/' className="p-[12px] bg-gray-400 rounded-[12px] text-black text-[14px]">홈으로</Link>
                 </div>
             </div>
