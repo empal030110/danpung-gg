a0f6ef4b44090cb8c378f68e7051c00f5cab1556
joominkyu <empal03@gmail.com>
2026-08-12 21:35:43 +0900

    feat: 잠재능력 줄임말 추가 (방무, 크확)

--- api/cutOptionName.ts
+++ api/cutOptionName.ts
@@ -1,5 +1,6 @@
 const optionNameMap: Record<string, string> = {
     '보스 몬스터 데미지': '보공',
+    '몬스터 방어율 무시': '방무',
     '스킬 재사용 대기시간': '쿨감',
     '크리티컬 데미지': '크뎀',
     '아이템 드롭률': '아획',
@@ -8,6 +9,7 @@ const optionNameMap: Record<string, string> = {
     '최대 MP': 'MP',
     '최대 HP': 'HP',
     'HP 회복 아이템 및 회복 스킬 효율': 'HP 효율',
+    '크리티컬 확률': '크확',
 };
 
 export const cutOptionName = (optionText?: string) => {
