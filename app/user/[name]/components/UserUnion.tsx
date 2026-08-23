61bc92253c0d318e4c41e20690523614bed9380f
joominkyu <empal03@gmail.com>
2026-08-23 17:50:02 +0900

    fix: 유니온 기본 정보 ui 변경

--- app/user/[name]/components/UserUnion.tsx
+++ app/user/[name]/components/UserUnion.tsx
@@ -2,8 +2,15 @@ import { userUnionProps } from "../../userProps/props";
 
 export default function UserUnion({ union }: { union: userUnionProps }) {
     return (
-        <p className="w-full flex justify-center items-center text-[16px] font-bold text-center">
-            {union.union_grade} | Lv.{union.union_level.toLocaleString()} | 아티팩트 Lv.{union.union_artifact_level}
-        </p>
+        <>
+            <p className="w-full hidden justify-center items-center text-[16px] font-bold text-center pc:flex">
+                {union.union_grade} | Lv.{union.union_level.toLocaleString()} | 아티팩트 Lv.{union.union_artifact_level}
+            </p>
+            <div className="w-full flex flex-col justify-center items-center gap-[6px] text-[16px] font-bold pc:hidden">
+                <p>{union.union_grade}</p>
+                <p>Lv.{union.union_level.toLocaleString()}</p>
+                <p>아티팩트 Lv.{union.union_artifact_level}</p>
+            </div>
+        </>
     );
 }
